5181bd43f09f31b5a48e5cb4a60c649aca04f436
Jack O'Connor <john.2.oconnor@cuanschutz.edu>
2024-06-06 09:46:27 -0600

    Modified Statistical Functions

--- Functions/euclidean.R
+++
@@ -1,2 +0,0 @@
-###This function scpecified a euclidean distance 
-euclidean <- function(a, b) sqrt(sum((a - b)^2))
--- Functions/stat_sum.R
+++
@@ -1,9 +0,0 @@
-###This function specifies a pairing field 
-stat_sum <- function(data, subset_vars, indep_var, depvars) {
-  pairing <- character(length(study_ids))  # Initialize an empty character vector
-  un_ids <- unique(study_ids)
-  for (i in seq_along(study_ids)) {
-    pairing[i] <- as.numeric(which(un_ids==study_ids[i]))
-  }
-  return(as.numeric(pairing))
-} 
